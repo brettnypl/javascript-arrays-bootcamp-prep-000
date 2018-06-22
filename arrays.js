22781ad97cebfd5e87c97ec57d5a16fbed7f6b20
Brett Portnoy <brett.portnoy@gmail.com>
2018-06-22 19:24:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,8 +31,9 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
 }
 
-function destructivelyRemoveElementFromEndOfArray() {
-  
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
 }
 
 function removeElementFromEndOfArray() {
