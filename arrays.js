ed8c10756478330132e7984c0efed54a01fec52a
Brett Portnoy <brett.portnoy@gmail.com>
2018-06-22 18:37:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array.unshift(element);
 }
 
-function addElementToEndOfArray() {
+function addElementToEndOfArray(array, element) {
   return [...array, element];
 }
 
