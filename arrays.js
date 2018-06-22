b22bc550a1e9b1105dfc43de53c75c8f4a75f3a0
Brett Portnoy <brett.portnoy@gmail.com>
2018-06-22 18:59:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,8 +22,9 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray() {
-  
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
 }
 
 function removeElementFromBeginningOfArray() {
