bab8fd284327e235d3bf7ed1de565cc65cc510a2
Brett Portnoy <brett.portnoy@gmail.com>
2018-06-22 18:25:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,32 +17,23 @@ function destructivelyAddElementToEndOfArray() {
 }
 
 function accessElementInArray() {
-  var presidents = ["washington", "adams", "jefferson"];
-  presidents[2];
+  
 }
 
 function destructivelyRemoveElementFromBeginningOfArray() {
-  var fruits = ["apple", "grapes", "pears"];
-  fruits.shift();
-  fruits;
+  
 }
 
 function removeElementFromBeginningOfArray() {
-  var veggies = ["avocado", "squash", "potato"];
-  veggies.slice(0);
-  veggies;
+  
 
 }
 
 function destructivelyRemoveElementFromEndOfArray() {
-  var yogurts = ["vanilla", "coffee", "strawberry"];
-  yogurts.pop();
-  yogurts;
+  
 }
 
 function removeElementFromEndOfArray() {
-  var junk = ["garbage", "nonsense", "trash"];
-  junk.slice(0, 2);
-  junk;
+  
 }
 
