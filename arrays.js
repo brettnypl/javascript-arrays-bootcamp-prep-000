f7b45fa9b4d1854a70c59d87b16405af4bff3e59
Brett Portnoy <brett.portnoy@gmail.com>
2018-06-22 18:17:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray() {
   var liverpoolPlayers = ["hendo", "bobby", "salah"];
   ["mane", ...liverpoolPlayers];
-  liverpoolPlayers;
 }
 
 function destructivelyAddElementToBeginningOfArray() {
